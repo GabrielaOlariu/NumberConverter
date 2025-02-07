37cdd94398d918f2882c73192b183f835a7eaf23
GabrielaOlariu <olariugabriela@gmail.com>
2025-02-07 11:29:51 +0100

    Add main menu

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("It's sunny outside")
+#develop these two functions
+
+   
+def introduce_developer():
+    print("Hello, My name is Gabriela, I developed this program.")
+    print("This application is called Number Converter.")
+
+def welcome_user():
+    print("Welcome!")
+    introduce_developer()
+
+def main_menu():
+    print("Option 1: reset app")
+    print("Option 2: binary converter")
+    print("Option 3: octal converter")
+    print("Option 4: decimal converter")
+    print("Option 5: hexadecimal converter")
+    print("Option 6: exit app")
+
+def start_program():
+    welcome_user()
+    main_menu()
+    pass
+
+start_program()
